0b119c8a383e2ed43bb83ef4865ce926198b15bf
teetarus <victortarus6@gmail.com>
2021-06-15 19:38:18 +0300

    requirements achieved

--- src/UI/Modal.js
+++ src/UI/Modal.js
@@ -3,7 +3,7 @@ import classes from './Modal.module.css'
 const Modal = (props) => {
     return (
         <>
-          <div className={classes.backdrop}></div>  
+          <div className={classes.backdrop} onClick={props.onClick}></div>  
           <div className={classes.modal}>
 {props.children}
           </div>
